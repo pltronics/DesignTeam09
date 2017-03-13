9fedb97a3f57193ed5f431dbdfb142d564876e5f
ybci2464 <pal12@zips.uakron.edu>
2017-03-13 15:45:00 -0400

    DesignTeam09

---
+++ DesignTeam09/RobotRuntime.java
@@ -0,0 +1,101 @@
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.File;
+import java.util.Scanner;
+
+public class RobotRuntime {
+    class pinout {
+	String path;
+	boolean forward;
+	short dutyCycle;
+	File direction;
+	public pinout (String s) {
+	
+	    path = s;
+	}
+
+    }
+
+    //class rearmotor
+
+    public static void main(String[] args) throws InterruptedException {
+	initialization();
+	try {
+	FileWriter value = new FileWriter("/sys/class/gpio/gpio4/value");
+	Scanner s = new Scanner(System.in);
+	int h=-1;
+	int l=-1;
+	System.out.println("Enter 1 time");
+	while(h == -1)
+		h = s.nextInt();
+	System.out.println("Enter 0 time");
+	while(l==-1)
+		l = s.nextInt();
+	while(true){
+		value.write("1");
+		value.flush();
+		for(int i = 0; i < h; i++)
+			Thread.sleep(1);
+//		long start = System.nanoTime();
+//		while(start + h >= System.nanoTime());
+
+		value.write("0");
+		value.flush();
+		for(int i = 0; i < l; i++)
+			Thread.sleep(1);
+//		start = System.nanoTime();
+//		while(start + l >= System.nanoTime());
+
+	}
+
+	} catch(IOException e){
+        e.printStackTrace();
+        }
+
+    }
+
+    private static void initialization(){
+	gpiostart("4");
+	gpiostart("14");
+	gpiostart("7");
+	gpiostart("17");
+	gpiostart("5");
+
+
+    }
+    
+    private static void gpiostart(String p){
+	try {
+	    FileWriter export = new FileWriter("/sys/class/gpio/export");
+	    File exportFileCheck = new File("/sys/class/gpio/gpio"+p);
+            if (exportFileCheck.exists()) {
+                gpioend(p);
+	    }
+	export.write(p);
+	export.flush();
+
+	FileWriter direction = new FileWriter("/sys/class/gpio/gpio"+p+"/direction");
+	direction.write("out");
+	direction.flush();
+
+	} catch(IOException e){
+	    e.printStackTrace();
+	}
+    }
+
+    private static void gpioend(String p) {
+	try {
+	    FileWriter unexport = new FileWriter("/sys/class/gpio/unexport");
+	    unexport.write(p);
+	    unexport.flush();
+	}catch(IOException e) {
+	    e.printStackTrace();
+	}
+
+
+    }   
+
+
+}
+
+
